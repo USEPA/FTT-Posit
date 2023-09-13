093456c5322aa800cfb652e12a355a8f078c9188
Joshua Powell <62256825+jpowel01@users.noreply.github.com>
2023-09-13 14:55:55 -0400

    Update FT_UI.R

--- FT_UI.R
+++ FT_UI.R
@@ -1,13 +1,13 @@
 source("tabsPanel.R")
 
+fn <- "anti-clickjacking.js"
+fnpath <- list.files(pattern = fn, full.names = TRUE, recursive = TRUE)
+
 FT_UI <- function(req) {
   fluidPage(
     useShinyjs(),
     tags$style(id = "antiClickjack", HTML("body{display:none !important;}")),
 
-
-    fn <- "anti-clickjacking.js",
-    fnpath <- list.files(pattern = fn, full.names = TRUE, recursive = TRUE),
     includeScript(fnpath, type = "text/javascript"),
 
     tags$script("$(document).on('shiny:connected', function(event) {
