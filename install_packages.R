48c263ff4c6c7f01ffec9baa5480d3f4141ebeae
Nathan Pollesch <pollesch.nathan@epa.gov>
2024-04-29 12:20:21 -0500

    Update install_packages.R
    Added line 'install.packages("shinyhelper")' to update use of this new pacakge.

--- install_packages.R
+++ install_packages.R
@@ -27,3 +27,4 @@ install.packages('Rmpfr', repos='https://cloud.r-project.org/')
 install.packages("magick") #not in use
 install.packages("devtools")
 install.packages("shiny.pwa")
+install.packages("shinyhelper")
