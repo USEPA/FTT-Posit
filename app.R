83d503d57a4f61ae6f58345bfaf58fd6085e8c8a
Pollesch <pollesch.nathan@epa.gov>
2025-01-27 12:14:28 -0600

    Updated R Package

--- app.R
+++ app.R
@@ -71,7 +71,7 @@ source("FT_UI.R")
 source("FT_Server.R")
 
 
-#configure Sentry error handling
+## configure Sentry error handling
 configure_sentry(dsn = "https://3d3c74380d1a43e73b33b78dee00fb27@ccte-app-monitoring.epa.gov/57",
                  app_name = "fish-tox-translator", app_version = "1.0.0")
 
