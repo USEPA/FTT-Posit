66f8067c531a60fb945bdf7475521c6d10ee78f5
Nathan Pollesch <pollesch.nathan@epa.gov>
2025-04-17 13:40:18 -0500

    Update Baseline_Tab_Events_Server.R

--- Baseline_Tab_Events_Server.R
+++ Baseline_Tab_Events_Server.R
@@ -873,7 +873,7 @@ output$rmark <- renderUI(
     {
       path_rmd <- "Species_Profile.Rmd"
       # Render into www/ folder.
-      path_html <- paste("www/",CurrentSpeciesName,"_Profile.html",sep = "")
+      path_html <- paste(CurrentSpeciesName,"_Profile.html",sep = "")
       render(
          path_rmd,
          output_format = "html_document",
