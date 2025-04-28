df3ff3b66ef3bbe400677608973aeb62a1bb00f8
Nathan Pollesch <pollesch.nathan@epa.gov>
2025-04-28 15:16:26 -0500

    Update Baseline_Tab_Events_Server.R
    updated for conditional in www/ www\\

--- Baseline_Tab_Events_Server.R
+++ Baseline_Tab_Events_Server.R
@@ -873,7 +873,9 @@ output$rmark <- renderUI(
     {
       path_rmd <- "Species_Profile.Rmd"
       # Render into www/ folder.
-      path_html <- paste("www/",CurrentSpeciesName,"_Profile.html",sep = "")
+      sname<-Sys.info()[["sysname]]
+      path_html<-ifelse(sname=="Windows", paste("www\\",CurrentSpeciesName,"_Profile.html",sep = ""),paste("www/",CurrentSpeciesName,"_Profile.html",sep = ""))
+      # path_html <- paste("www/",CurrentSpeciesName,"_Profile.html",sep = "")
       render(
          path_rmd,
          output_format = "html_document",
