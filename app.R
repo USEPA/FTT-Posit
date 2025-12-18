1a3e267d3a17cd15973ac6eb8931e078be73fdf2
Nathan Pollesch <pollesch.nathan@epa.gov>
2025-12-18 09:56:58 -0600

    update commented out sentry config.  Add updated markdown docs.  No coding changes.

--- app.R
+++ app.R
@@ -78,15 +78,15 @@ source("FT_UI.R")
 source("FT_Server.R")
 
 
-## configure Sentry error handling
-configure_sentry(dsn = "https://3d3c74380d1a43e73b33b78dee00fb27@ccte-app-monitoring.epa.gov/57",
-                 app_name = "fish-tox-translator", app_version = "1.0.0")
-
-error_handler <- function() {
-  capture_exception(error = geterrmessage())
-}
-
-options(shiny.error = error_handler)
+# ## configure Sentry error handling
+# configure_sentry(dsn = "https://3d3c74380d1a43e73b33b78dee00fb27@ccte-app-monitoring.epa.gov/57",
+#                  app_name = "fish-tox-translator", app_version = "1.0.0")
+# 
+# error_handler <- function() {
+#   capture_exception(error = geterrmessage())
+# }
+# 
+# options(shiny.error = error_handler)
 
 # This runs the Shiny App!
 shinyApp(ui <- FT_UI, server <- FT_Server)
